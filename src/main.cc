822ba4960692ff91f632658915b829ce164a2497
Nicolas Mémeint <nicomem@pm.me>
2021-02-17 18:56:58 +0100

    Change invalid args help message

--- src/main.cc
+++ src/main.cc
@@ -8,7 +8,15 @@ int main(int argc, char *argv[])
 {
   if (argc < 3)
   {
-    std::cerr << argv[0] << " input.png output.png [N_iter [T_init [T_dec_factor [normal_std]]]]\n";
+    std::cerr << argv[0] << " input.png output.png [PARAMS]\n"
+              << "  where PARAMS are the following optional arguments (in that order):\n";
+
+    constexpr const char *opt_args[] = {"N_iter", "T_init", "T_dec_factor", "normal_std"};
+    for (const auto &arg : opt_args)
+    {
+      std::cerr << "    - " << arg << '\n';
+    }
+
     return EXIT_FAILURE;
   }
 
